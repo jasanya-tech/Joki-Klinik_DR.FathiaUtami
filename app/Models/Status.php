6ee157bdf6a99d22a0761e8958bfd8e038c1c1d1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-10 16:12:30 +0700

    buat table status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\StatusType;
+use App\Traits\AuditedBySoftDelete;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Status extends Model
 {
-    //
+    use HasFactory, Notifiable, SoftDeletes, AuditedBySoftDelete;
+    protected $table = 'status';
+    protected $guarded = ['id'];
+
+    public function statusType()
+    {
+        return $this->belongsTo(StatusType::class);
+    }
 }
--- database/migrations/2025_05_10_090927_create_statuses_table.php
+++ database/migrations/2025_05_10_090927_create_statuses_table.php
@@ -1,19 +1,24 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use App\Traits\BaseModelSoftDeleteDefault;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
+    use BaseModelSoftDeleteDefault;
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('statuses', function (Blueprint $table) {
+        Schema::create('status', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('status_type_id')->constrained('status_type');
+            $table->string('name', 128);
+            $table->string('desc', 255)->nullable();
+            $this->base($table);
         });
     }
 
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('statuses');
+        Schema::dropIfExists('status');
     }
 };
