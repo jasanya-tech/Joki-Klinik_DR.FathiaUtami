b8af2c9fedd5f9e7ba7f8e6f1426738f3430bb3a
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-10 15:58:26 +0700

    update table users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,13 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
+            $table->string('avatar_url', 255)->nullable(false);
+            $table->string('phone_number', 16)->nullable(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
-
+            
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
