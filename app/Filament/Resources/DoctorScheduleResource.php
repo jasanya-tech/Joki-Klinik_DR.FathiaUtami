df81994c5bafb75a53a88c2925a3995fb312ddac
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-14 16:05:16 +0700

    buat resource doctor schedule

---
+++ app/Filament/Resources/DoctorScheduleResource.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DoctorScheduleResource\Pages;
+use App\Filament\Resources\DoctorScheduleResource\RelationManagers;
+use App\Models\DoctorSchedule;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DoctorScheduleResource extends Resource
+{
+    protected static ?string $model = DoctorSchedule::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('doctor_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('day')
+                    ->required(),
+                Forms\Components\TextInput::make('start_time')
+                    ->required(),
+                Forms\Components\TextInput::make('end_time')
+                    ->required(),
+                Forms\Components\TextInput::make('status_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('created_by')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                Forms\Components\TextInput::make('updated_by')
+                    ->numeric(),
+                Forms\Components\TextInput::make('deleted_by')
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('doctor_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('day'),
+                Tables\Columns\TextColumn::make('start_time'),
+                Tables\Columns\TextColumn::make('end_time'),
+                Tables\Columns\TextColumn::make('status_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updated_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDoctorSchedules::route('/'),
+            'create' => Pages\CreateDoctorSchedule::route('/create'),
+            'edit' => Pages\EditDoctorSchedule::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/DoctorScheduleResource/Pages/EditDoctorSchedule.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\DoctorScheduleResource\Pages;
+
+use App\Filament\Resources\DoctorScheduleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDoctorSchedule extends EditRecord
+{
+    protected static string $resource = DoctorScheduleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
