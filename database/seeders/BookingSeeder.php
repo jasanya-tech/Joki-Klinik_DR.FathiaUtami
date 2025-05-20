d7b62b225e74708d9af0cff9c580d559634720f2
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-20 10:39:24 +0700

    buat booking seeder

---
+++ database/seeders/BookingSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Booking;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $booking = 
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'doctor_schedule_id' => 1,
+                'complaint' => 'Badan saya terasa lemas dan tidak bertenaga',
+                'doctor_feedback' => 'Dokter nya sangat ramah dan berkata bijak',
+                'status_id' => 1,
+            ];
+        Booking::insert($booking);
+    }
+}
