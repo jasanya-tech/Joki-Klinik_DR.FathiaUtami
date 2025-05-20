94859a9f31d13af6041966f50cdca450be6852a4
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-20 10:40:47 +0700

    tambah class di database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StatusSeeder::class,
             UserSeeder::class,
+            DoctorSeeder::class,
+            BookingSeeder::class,
+            BlogSeeder::class,
         ]);
     }
 }
