e04da19c301141c429f98fd1f0dc7c88d4436d08
drymhd <darmah250903@gmail.com>
2025-06-02 13:16:16 +0700

    fix: migration

--- database/migrations/2025_06_02_041239_add_desc_doctor.php
+++ database/migrations/2025_06_02_041239_add_desc_doctor.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('doctor', function (Blueprint $table) {
-            $table->text('desc')->nullable();
+            $table->dropColumn('desc');
         });
     }
 };
