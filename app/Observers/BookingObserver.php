abb0e9d4eeb28a07de51649f60dbdb7d338c92b1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-06 18:35:53 +0700

    buat creating booking di observer

--- app/Observers/BookingObserver.php
+++ app/Observers/BookingObserver.php
@@ -3,15 +3,34 @@
 namespace App\Observers;
 
 use App\Models\Booking;
+use App\Models\DoctorSchedule;
+use Illuminate\Support\Carbon;
 
 class BookingObserver
 {
     /**
      * Handle the Booking "created" event.
      */
-    public function created(Booking $booking): void
+    public function creating(Booking $booking)
     {
-        //
+        $schedule = DoctorSchedule::findOrFail($booking->doctor_schedule_id);
+
+        // Pastikan booking_date ter-parse
+        $today = Carbon::parse($booking->booking_date)->toDateString();
+
+        $lastQueue = Booking::whereDate('booking_date', $today)
+            ->where('doctor_schedule_id', $booking->doctor_schedule_id)
+            ->max('queue_number');
+
+        $queueNumber = $lastQueue ? $lastQueue + 1 : 1;
+        $booking->queue_number = $queueNumber;
+
+        $estimatedTime = Carbon::parse($schedule->start_time)
+            ->addMinutes(15 * ($queueNumber - 1));
+        $booking->estimated_time = $estimatedTime->format('H:i');
+
+        $randomCode = strtoupper(Str::random(5)) . now()->format('Ymd');
+        $booking->code = 'BOOK-' . $randomCode;
     }
 
     /**
