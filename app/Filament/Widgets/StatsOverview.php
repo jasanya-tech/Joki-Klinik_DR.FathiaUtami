48db386d3185f3dd75c3f846e1f7d517d7211240
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-11 22:19:52 +0700

    fix stats widget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -13,22 +13,22 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        $user = User::count();
+        $user = User::count('role', 'user');
         $blog = Blog::count();
         $doctor = User::where('role', 'doctor')->count();
 
         return [
-            Stat::make('Jumlah Konten', '242')
+            Stat::make('Jumlah Konten', $blog)
                 ->description('Konten')
                 ->descriptionIcon('heroicon-m-pencil')
                 ->chart([2, 3, 35, 18, 15, 26, 15, 30, 25, 30, 25, 50])
                 ->color('success'),
-            Stat::make('Jumlah Dokter', '23')
+            Stat::make('Jumlah Dokter', $doctor)
                 ->description('Dokter')
                 ->descriptionIcon('heroicon-m-heart')
                 ->chart([32, 23, 35, 18, 15, 56, 15, 30, 25, 30, 25, 30])
                 ->color('warning'),
-            Stat::make('Jumlah Pasien', '17')
+            Stat::make('Jumlah Pasien', '244')
                 ->description('Pasien')
                 ->descriptionIcon('heroicon-m-users')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
