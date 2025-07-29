e11dbbb98709e4c237e375901111b38acf2b1473
WahyuTomo14 <wahyuxd14@gmail.com>
2025-07-29 09:30:45 +0700

    tambah with trashed di relasi user

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -21,7 +21,7 @@ class Booking extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id')->withTrashed();
     }
 
     public function doctor()
